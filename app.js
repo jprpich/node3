c18fc2e5b26d27d6a719567334fabe472e8a504c
Joshua <joshuaprpich@gmail.com>
2019-04-08 23:13:33 -0400

    change to params

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  if (req.query.nombre){
-    res.send("<h1>Hola "+ req.query.nombre + "!</h1>")
-  } else {
-    res.send("<h1>Hola desconocido!</h1>")
-  }
+app.get('/makers/:nombre', (req, res) => {
+    res.send("<h1>Hola "+ req.params.nombre.charAt(0).toUpperCase() + req.params.nombre.slice(1) + "!</h1>")
 })
 
 app.listen(3000, ()=> console.log("ready"));
